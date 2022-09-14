5e1f32f894135bbf5f3a1ee21d9680b26e1c2313
FlorianLauv <f.lauv@codeur.online>
2022-09-14 16:21:03 +0200

    sidebar changes

--- views/sidebarView.php
+++ views/sidebarView.php
@@ -10,24 +10,42 @@
 </head>
 
 <body>
-    
+
     <section class="sidebar">
-        <a href="#openModal">
-            <button class="new" for="modal-content">
-            <img src="/assets/img/add.png">
-            Créer
-            </button>
-        </a>
+        <div><!-- <a>
+                <button>
+                    <img src="/assets/img/add.png">
+                </button></a> -->
+                <h2>Agenda</h2>
+        </div>
+                <div class="dropdown-header">
+                <div class="">Trier</div>
+
+                <div class="div-button">
+                    <button class="button"><img src="../assets/img/down-arrow.png"></button>
+                    <div class="dropdown-content">
+                        <a href="#">Du plus récent</a>
+                        <a href="#">Du plus ancien</a>
+                        <a href="#">Evènement le plus récent</a>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div><input class="publicCheck" type="checkbox">Public</div>
+                <div><input class="privateCheck" type="checkbox">Privé</div>
+                <div><input class="createCheck" type="checkbox">Créateur</div>
+                <div class="guestCheck"><input  type="checkbox">Invité</div>
+            </div>
+        </div>
         <hr>
-        
-        <div class="calendar">
+
+       <!-- <div class="calendar">
             <div class="month">
                 <div>Septembre</div>
-                <div class="pre"><</div>
-                <div class="nex">></div>
+                <div class=""> < ></div>
             </div>
-
-             <div class="grid_numbers">
+                <br>
+                <div class="grid_numbers">
                     <div>1</div>
                     <div>2</div>
                     <div>3</div>
@@ -58,73 +76,117 @@
                     <div>28</div>
                     <div>29</div>
                     <div>30</div>
+                </div>
             </div>
-        </div>
-        <hr>
-        
-        <div>
-            <button class="button" href="#">Filtrer<img src="/assets/img/down-arrow.png">
-            </button>
-        </div>
-        <div>
-            <div><input class="" type="checkbox">Tout</div>
-            <div><input class="" type="checkbox">Rendez-vous</div>
-            <div><input class="" type="checkbox">Anniversaires</div>
-            <div><input class="" type="checkbox">Rappels</div>
-        </div>
+            <hr> -->
+            <div><a href="#openModal">
+                    <h2 class="new" for="modal-content">Evènements</h2>
+                    </button></a>
+            </div>
+            <div class="dropdown-header">
+                <div class="">Afficher</div>
+
+                <div class="div-button">
+                    <button class="button"><img src="../assets/img/down-arrow.png"></button>
+                    <div class="dropdown-content">
+                        <a href="#">Tous les jours</a>
+                        <a href="#">Jours avec des évènements</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="dropdown-header">
+                <div class="">Trier</div>
+
+                <div class="div-button">
+                    <button class="button"><img src="../assets/img/down-arrow.png"></button>
+                    <div class="dropdown-content">
+                    <a href="#">Du plus récent</a>
+                    <a href="#">Du plus ancien</a>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div class="allCheck"><input type="checkbox">Tout</div>
+                <div class="appointCheck"><input type="checkbox">Rendez-vous</div>
+                <div class="birthCheck"><input type="checkbox">Anniversaires</div>
+                <div class="reminderCheck"><input type="checkbox">Rappels</div>
+            
     </section>
 
-<div id="openModal" class="modalDialog">
-    <div>
-        <header>Créer un évènement</header>
-        <a href="#close" title="Close" class="close">X</a>
-        <span>
-            <hr>
-        </span>
-            <title>Créer un évènement</title>
-            <input type="text" placeholder="Nom de l'évènement">
-            <br>
-            <label for="category">
-                <select name="category" id="category">
-                    <option value="Reunion">Réunion</option>
-                    <option value="Anniversaire">Anniversaire</option>
-                    <option value="RDV">Rendez-vous</option>
-                    <option value="Tache">Tâche</option>
-                </select>
-            </label>
-            <section class="date">
-                <div><img src="/assets/img/clock.png"></div>
-                <div>
-                    <input type="date" id="start" name="" value="2022-09-07" min="2022-01-01" max="2022-12-31">
-                </div>
-                <div><input id="allday" type="checkbox">Toute la journée</div>
-                <div class="hours">
-                    <input type="time" name="" value="14:12">
-                </div>
-                <div class="hours">
-                    <input type="time" name="" value="15:12">
-                </div>
-            </section>
-            <section class="reminder">
-                <div>
-                    <img src="/assets/img/friend.png">
-                    <button>Inviter des amis</button>
-                </div>
-                <div>
-                    <input class="" type="checkbox">Rappel                    
-                        <select name="reminder" id="reminder">
+    <div id="openModal" class="modalDialog">
+        <div>
+            <header>Créer un évènement</header>
+            <a href="#close" title="Fermer" class="close">X</a>
+            <div class="modal-content">
+                <span>
+                    <hr>
+                </span>
+                <title>Créer un évènement</title>
+                <input type="text" placeholder="Nom de l'évènement" reset>
+                <br>
+                <label for="category">
+                    <select name="category" id="category">
+                        <option value="Reunion">Réunion</option>
+                        <option value="Anniversaire">Anniversaire</option>
+                        <option value="RDV">Rendez-vous</option>
+                        <option value="Tache">Tâche</option>
+                    </select>
+                </label>
+                <section class="date">
+                    <div><img src="/assets/img/clock.png">
+                        <input type="date" name="" value="2022-09-07" min="2022-01-01" max="2022-12-31">
+                    </div>
+                    <div class="centercheckbox">
+                        <input id="allday" type="checkbox">Journée
+                        <div class="day">
+                            <input class="" type="time" name="" value="14:12">
+                            <input class="" type="time" name="" value="15:12">
+                        </div>
+                    </div>
+                </section>
+                <section class="reminder">
+                    <div>
+                        <img src="/assets/img/add-user.png">
+                        <button>Inviter des amis</button>
+                    </div>
+                    <div class="centercheckbox">
+                        <input class="" type="checkbox" id="reminder" checked>Rappel
+                        <select name="reminder" class="rappel">
                             <option value="minute">30 minutes avant</option>
                             <option value="hour">1 heure avant</option>
                             <option value="2hour">2 heures avant</option>
                             <option value="day">1 jour avant</option>
-                        </select>                    
-                </div>
-            </section>
-            <textarea name="" cols="60" rows="10"></textarea>
-            <button>Enregistrer</button>
-   </div>
-</div>
-
-</body>
+                        </select>
+                    </div>
+                </section>
+                <div class="description" placeholder="description">Ajoutez une description</div>
+                <textarea name="" cols="60" rows="10"></textarea>
+                <button class="event">Enregistrer</button>
+            </div>
+        </div>
+    </div>
+    
+    <section class="content">
+        <div class="month">
+            <span>Lundi</span>
+            <span>Mardi</span>
+            <span>Mercredi</span>
+            <span>Jeudi</span>
+            <span>Vendredi</span>
+            <span>Samedi</span>
+            <span>Dimanche</span>
+            <?php
+            for ($i = 1; $i <=35; $i++) {
+                if ($i >31){ $j = $i -31;
+                     echo "<div>$j</div>";}
+                else echo "<div>$i</div>";
+                }
+            ?>
+        </div>
+    </section>
+    </div>
+    </body>
 
 </html>
